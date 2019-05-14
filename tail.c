9ccb6df3c02b066904402e1476e859864b5735e9
Vincent Nikolayev <vincent.nikolayev@gmail.com>
2019-05-14 05:11:29 -0400

    tail working, freeing memory does not

--- tail.c
+++ tail.c
@@ -2,38 +2,69 @@
 #include "stat.h"
 #include "user.h"
 
+#define NULL 0
+
 char buff[512];
 
-struct Cyclic_Arr
+struct CircularArr
 {
     void **arr;
     uint size;
     uint index; // refers to next free spot
 };
 
-void addElem(struct Cyclic_Arr *lines, void *const line)
+// adds an element, sets index to the beginning if size is passed
+void addElem(struct CircularArr *lines, void *const line)
 {
+    // free(lines->arr[lines->index]);
+    // lines->arr[lines->index] = NULL;
     lines->arr[lines->index] = line;
     if (++lines->index >= lines->size)
         lines->index = 0;
 }
 
-int getLast(struct Cyclic_Arr const *lines)
+// retuns the index of the last inserted elem
+int getLast(struct CircularArr const *lines)
 {
     if (lines->index == 0)
         return lines->size - 1;
     return lines->index;
 }
 
-void appendLast(struct Cyclic_Arr *lines, char *const line)
+char* strcat(char* dest, char* src) 
 {
-    char *lastLine = lines->arr[getLast(lines)];
-    char *newLine = malloc(strlen(lastLine) + strlen(line) + 1); // + 1 for null
-    strcat(newLine, lastLine);
-    strcat(newLine, line);
+    // make ptr point to the end of destination string
+    char *ptr = dest + strlen(dest);
+
+    // Appends characters of src to the dest string
+    while (*src != '\0')
+        *ptr++ = *src++;
+
+    // null terminate dest string
+    *ptr = '\0';
+
+    // dest is returned by standard strcat()
+    return dest;
 }
 
-void printInOrder(struct Cyclic_Arr *const lines)
+// appends a string to the last inserted string
+void appendLast(struct CircularArr *lines, char *const line)
+{
+    int lastI = getLast(lines);
+    char *lastLine = lines->arr[lastI];
+    char *newLine = malloc(strlen(lastLine) + strlen(line) + 1); // + 1 for null
+    newLine[0] = '\0';
+    strcat(newLine, lastLine);
+    strcat(newLine, line);
+
+    free(line);
+    free(lastLine);
+
+    lines->arr[lastI] = newLine;
+}
+
+// prints the elements in the correct order from first inserted to last inserted
+void printInOrder(struct CircularArr *const lines)
 {
     int i, start;
     i = start = lines->index;
@@ -41,21 +72,31 @@ void printInOrder(struct Cyclic_Arr *const lines)
     int first = 1;
     while (i != start || first) // first is there so it executes in the beginning, then it iterates until the end and loops to the beginning
     {
-        printf(1, "%s\n", lines->arr[i]);
+        printf(1, "%s", lines->arr[i]);
         if (++i >= lines->size)
             i = 0;
         first = 0;
     }
 }
 
+// void init(struct CircularArr *lines)
+// {
+//     int i;
+//     for (i = 0; i < lines->size; i++)
+//     {
+//         lines->arr[i] = malloc(NULL);
+//     }
+// }
+
 void tail(int fd, uint limit)
 {
     int n, i, start, numBytes;
-    struct Cyclic_Arr lines = {
+    struct CircularArr lines = {
         malloc(sizeof(char **) * limit),
         limit,
         0,
     }; // allocate an arr of size limit which stores lines
+    init(&lines);
 
     while ((n = read(fd, buff, sizeof(buff))) > 0)
     {
@@ -70,14 +111,14 @@ void tail(int fd, uint limit)
 
                 addElem(&lines, line);
             }
-            else if (i == n - 1)
+            else if (i == n - 1) // end of buffer will be reached before reading in another new line
             { // end of arr reached
                 char *line = malloc(sizeof(char *) * numBytes);
                 memmove(line, &buff[start], n - start);
                 start = i + 1; // move ahead one index to after newline
                 numBytes = 0;  // reset numBytes (numBytes will immediately become 1 on next iteration)
 
-                appendLast(&lines, line);
+                appendLast(&lines, line); // have to append to the last inserted entry since no new line was found
             }
         }
     }
